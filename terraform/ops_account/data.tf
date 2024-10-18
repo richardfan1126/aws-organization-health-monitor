1ee7dd032cf0ca9570835a53849ee23054efb417
Richard Fan <richardfan1126@gmail.com>
2024-10-18 11:17:20 +0800

    Add random suffix to project name

--- terraform/ops_account/data.tf
+++ terraform/ops_account/data.tf
@@ -6,6 +6,14 @@ data "aws_chatbot_slack_workspace" "main" {
   slack_team_name = local.slack_workspace_name
 }
 
+resource "random_string" "random" {
+  length  = 4
+  special = false
+  lower   = true
+  upper   = false
+  numeric = true
+}
+
 locals {
   member_account_ids = toset(data.aws_organizations_organization.main.non_master_accounts[*].id)
   all_account_ids    = toset(data.aws_organizations_organization.main.accounts[*].id)
@@ -15,7 +23,7 @@ locals {
   slack_workspace_name = local.config["slack_workspace_name"]
   deployment_regions   = local.config["deployment_regions"]
 
-  project_name            = "account-health-notification"
+  project_name            = "account-health-notification-${random_string.random.id}"
   stackset_name           = "${local.project_name}-stackset"
   stackset_exec_role_name = "${local.project_name}-stackset-exec-role"
 
